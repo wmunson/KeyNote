1a8e5780db1a5ee37514f7affbaebdc8ce67b17f
Wes <munsonwa@gmail.com>
2017-02-16 15:08:47 -0500

    home html fix

--- static/js/login.js
+++ static/js/login.js
@@ -1,23 +1,17 @@
 
 
-require.config({
-  baseUrl: '/js',
-  paths: {
-    d3: "http://d3js.org/d3.v3.min"
+document.getElementById('signUp').addEventListener("click",
+  function(){
+    document.getElementById('changer1').classList.remove("active");
+    document.getElementById('changer2').className += " active";
+     
   }
-});
+);
 
-require(["d3", "c3"], function(d3, c3) {
-  c3.generate({
-    
-    bindto: '#chart',
-    data: {
-      columns: [
-        ['data1', 30, 200, 100, 400, 150, 250],
-        ['data2', 50, 20, 10, 40, 15, 25]
-      ]
-    }
-
-
-  });
-});
+document.getElementById('loginUp').addEventListener("click",
+  function(){
+    document.getElementById('changer2').classList.remove("active");
+    document.getElementById('changer1').className += " active";
+     
+  }
+);
