5cdef55571359da253b5523c86f6429c879e6205
sashapotash29 <sashapotash29@gmail.com>
2017-02-17 11:58:38 -0500

    price_list routes

--- tools.py
+++ tools.py
@@ -26,7 +26,8 @@ def session_set(user):
 def make_stock_list(ticker):
 	ticker = ticker.upper()
 	print(ticker)
-	url = "http://chart.finance.yahoo.com/table.csv?s=IBM&a=0&b=17&c=2017&d=1&e=17&f=2017&g=d&ignore=.csv"
+	# url = "http://chart.finance.yahoo.com/table.csv?s="+ticker+"&a=0&b=17&c=2017&d=1&e=17&f=2017&g=d&ignore=.csv"
+	url = "http://chart.finance.yahoo.com/table.csv?s="+'^GSPC'+"&a=0&b=17&c=2017&d=1&e=17&f=2017&g=d&ignore=.csv"
 	# url = "http://chart.finance.yahoo.com/table.csv?s="+ticker+"&a=0&b=7&c=2007&d=1&e=17&f=2017&g=d&ignore=.csv"
 	s=requests.get(url).content
 	
@@ -38,10 +39,22 @@ def make_stock_list(ticker):
 	dataframe = pd.read_csv(io.StringIO(s.decode('utf-8')))
 
 	price_list = []
-
+	date_list = []
+	print(dataframe)
 	for index, row in dataframe.iterrows():
 		price_list.append(row.Close)
-	return price_list
+		date_list.append(row.Date)
+	final_product = {
+			"company" : 'GSPC',
+			"price_list": price_list,
+			"date_list": date_list
+	}
+	print(price_list)
+	print(date_list)
+	print(len(price_list))
+	print(len(date_list))
+	print(final_product)
+	return final_product
 
 
 
