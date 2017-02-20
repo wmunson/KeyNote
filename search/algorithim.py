63d9c20e1ed7532c70bed0f711c45be61a164f29
sashapotash29 <sashapotash29@gmail.com>
2017-02-20 14:17:27 -0500

    whoops

--- search/algorithim.py
+++ search/algorithim.py
@@ -4,7 +4,7 @@ import requests
 
 headers = {'Authorization': 'Basic $BASE64_ENCODED(026b77b5e5bc57d6828f77e0d7264181:dbc7c510b2fa7e304dc5cccbb8b97768)'}
 url = 'https://api.intrinio.com/data_point?ticker=AAPL&item=close_price'
-x=requests.get(url, headers=headers).content
+x=requests.get(url, auth=('026b77b5e5bc57d6828f77e0d7264181','dbc7c510b2fa7e304dc5cccbb8b97768')).content
 print(x)
 print(type(x))
 print(dir(x))
