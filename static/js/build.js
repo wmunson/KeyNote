8501c0bcbfbf9ecaa55728babcf9093e19608488
Wes <munsonwa@gmail.com>
2017-02-21 17:55:49 -0500

    search bar

--- static/js/build.js
+++ static/js/build.js
@@ -1,4 +1,8 @@
 
+///////////////////////////////////////////////
+/////////pie chart///////////////////////////
+/////////////////////////////////////////////
+
 require.config({
 			baseUrl: '/js',
 			paths: {
@@ -7,23 +11,39 @@ require.config({
 		});
 require(["d3", "c3"], function(d3, c3) {
   			
-var chart = c3.generate({
-    bindto: '.pieChart',
-    data: {
-        columns: [
-            ["setosa", 0.2],
-            ["versicolor", 1.4],
-            ["virginica", 2.5],
-        ],
-        type : 'donut',
-        onclick: function (d, i) { console.log("onclick", d, i); },
-        onmouseover: function (d, i) { console.log("onmouseover", d, i); },
-        onmouseout: function (d, i) { console.log("onmouseout", d, i); }
-    },
-    donut: {
-        title: "Techie"
-    }
+	var chart = c3.generate({
+	    bindto: '.pieChart',
+	    data: {
+	        columns: [
+	            ["setosa", 0.2],
+	            ["versicolor", 1.4],
+	            ["virginica", 2.5],
+	        ],
+	        type : 'donut',
+	        onclick: function (d, i) { console.log("onclick", d, i); },
+	        onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+	        onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+	    },
+	    donut: {
+	        title: "Techie"
+	    }
+	});
+
+
+});
+
+//////////////////////////////////////////////
+/////search stocks functions///////////////
+////////////////////////////////////////////
+
+document.getElementById('searchStocks').addEventListener('keyup', function(event){
+	event.preventDefault()
+	if(event.keyCode == 13){ 
+	console.log('good');
+	}
 });
 
 
-	  			});
+const searchStock = function(event){
+
+}
