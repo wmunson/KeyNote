352781fd81169dbc7e2ef1d31dca799c149dc81f
Wes <munsonwa@gmail.com>
2017-02-21 15:07:25 -0500

    z-index fixes

---
+++ static/js/build.js
@@ -0,0 +1,29 @@
+
+require.config({
+			baseUrl: '/js',
+			paths: {
+    		d3: "http://d3js.org/d3.v3.min"
+  			}
+		});
+require(["d3", "c3"], function(d3, c3) {
+  			
+var chart = c3.generate({
+    bindto: '.pieChart',
+    data: {
+        columns: [
+            ["setosa", 0.2],
+            ["versicolor", 1.4],
+            ["virginica", 2.5],
+        ],
+        type : 'donut',
+        onclick: function (d, i) { console.log("onclick", d, i); },
+        onmouseover: function (d, i) { console.log("onmouseover", d, i); },
+        onmouseout: function (d, i) { console.log("onmouseout", d, i); }
+    },
+    donut: {
+        title: "Techie"
+    }
+});
+
+
+	  			});
