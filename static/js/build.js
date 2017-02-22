13f754c23dcd3707442d8f8413db0795c08cc3f7
Wes <munsonwa@gmail.com>
2017-02-22 14:23:10 -0500

    search stuff

--- static/js/build.js
+++ static/js/build.js
@@ -39,23 +39,68 @@ require(["d3", "c3"], function(d3, c3) {
 document.getElementById('searchStocks').addEventListener('keyup', function(event){
 	event.preventDefault()
 	if(event.keyCode == 13){ 
-	var name=this.value
-	console.log(this.value);
+	var name=this.value;
+	console.log(name);
 	searchStock(name);
+	// stocksToDom(stocks);
 	}
 });
 
 
 const searchStock = function(name){
 // console.log(name)	
-var xhttp = new XMLHttpRequest();
-	xhttp.onreadystatechange = function(){
-		if (this.readyState == 4 && this.status == 200){
-			var data = JSON.parse(this.responseText);
-			console.log(data)
-		}
-	};
-	xhttp.open("GET", "", true);
-	xhttp.send();
+	var url = "/stock/"+name;
+	var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function(){
+			if (this.readyState == 4 && this.status == 200){
+				var data = JSON.parse(this.responseText);
+				console.log(data)
+				stocksToDom(data)
+			}
+		};
+		xhttp.open("GET", url, true);
+		xhttp.send();
+
 }
+
+
+const stocksToDom = function(stocks){
+	var ul = document.getElementById('stockUl');
+	
+	
+	var arr = []
+	for (i=0;i<stocks.length;i++){
+		const li = document.createElement("li");
+		li.setAttribute('class','stockTick')
+		li.setAttribute('id',stocks[i].Symbol)
+		
+		if (stocks[i].Name){
+			const name = document.createTextNode(stocks[i].Name);
+			li.appendChild(name);
+			addEvent(li)
+			
+			ul.appendChild(li);
+		}
+		else{
+			const symbol = document.createTextNode(stocks[i].Symbol);
+			li.appendChild(symbol);
+			addEvent(li)
+
+			ul.appendChild(li);
+		}
+	}
+}
+
+
+const addEvent=function(li){
+	li.addEventListener('click',function(){
+	var tick = this.id
+	
+	})
+}
+
+
+// document.querySelector('.stockTick').addEventListener('click',function(){
+// 	console.log(this.id)
+// })
 
