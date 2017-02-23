4677fbd01bea94b0e77c8fa7f278a3dae3161c30
sashapotash29 <sashapotash29@gmail.com>
2017-02-23 14:30:39 -0500

    symbol for JS

--- app.py
+++ app.py
@@ -247,10 +247,11 @@ def grab_stock_info(ticker):
 	else:
 		input_url = 'http://dev.markitondemand.com/MODApis/Api/v2/Quote/json?symbol=' + ticker.upper()
 		stock_info = requests.get(input_url).json()
-		if stock_info['Status'] == "SUCCESS"
+		if stock_info['Status'] == "SUCCESS":
 			return json.dumps(stock_info)
 		else:
-			return json.dumps({'void': "Sorry the information is not available."})
+			return json.dumps({'Symbol': stock_info['Symbol'],
+				'void': "Sorry the information is not available."})
 
 
 if __name__ == "__main__":
